8e12f30545fc2fe1fa0f81de308d728ad41efdc1
ashrithyvs <ashrithy20@gmail.com>
2021-08-29 23:32:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Button, Container, Spinner } from "react-bootstrap";
+import Pokemon from "./components/Pokemon/Pokemon";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [pokedex, setAllPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
   );
-}
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    setLoadMore(data.next);
+
+    function createPokemonObject(results) {
+      results.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+        setAllPokemons((currentList) => [...currentList, data]);
+        await pokedex.sort((a, b) => a.id - b.id);
+        setLoading(false);
+      });
+    }
+    createPokemonObject(data.results);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  return (
+    <Container className="text-center my-5">
+      <h1>The Pokedex</h1>
+      <div className="pokemon-container">
+        <div className="d-flex flex-wrap justify-content-center">
+          {!loading ? (
+            pokedex.map((pokemon, index) => (
+              <Pokemon
+                key={index}
+                image={pokemon.sprites.other.dream_world.front_default}
+                name={pokemon.name}
+                weight={pokemon.weight}
+                type={pokemon.types[0].type.name}
+                move={pokemon.moves[2].move.name}
+              />
+            ))
+          ) : (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          )}
+        </div>
+        <Button variant="outline-dark" onClick={() => getAllPokemons()}>
+          Load more
+        </Button>
+      </div>
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/components/Pokemon/Pokemon.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import "./Pokemon.css";
+const Pokemon = ({ image, name, type, move, weight }) => {
+  return (
+    <Card className={`p-1 m-3 ${type}`} style={{ width: "10rem" }}>
+      <div className="my-auto">
+        <Card.Img className="img" variant="top" src={image} alt={name} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text className="d-flex flex-column">
+            <small>Type: {type}</small>
+            <small>Weight: {weight}</small>
+            <small>Move: {move}</small>
+          </Card.Text>
+        </Card.Body>
+      </div>
+    </Card>
+  );
+};
+
+export default Pokemon;
