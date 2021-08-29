7942f6c213a9639c4495f61ddbccd6cc1d23b114
ashrithyvs <ashrithy20@gmail.com>
2021-08-29 23:48:15 +0530

    Minor fixes

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -3,12 +3,12 @@ import { Card } from "react-bootstrap";
 import "./Pokemon.css";
 const Pokemon = ({ image, name, type, move, weight }) => {
   return (
-    <Card className={`p-1 m-3 ${type}`} style={{ width: "10rem" }}>
+    <Card className={`py-2 px-1 m-3 shadow ${type}`} style={{ width: "10rem" }}>
       <div className="my-auto">
         <Card.Img className="img" variant="top" src={image} alt={name} />
         <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text className="d-flex flex-column">
+          <Card.Title className="name">{name}</Card.Title>
+          <Card.Text className="d-flex flex-column ">
             <small>Type: {type}</small>
             <small>Weight: {weight}</small>
             <small>Move: {move}</small>
